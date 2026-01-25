34452429c0066f5ae20329585d2555eb7c3a83e5
Azmain01 <anowarulazmainkhan@gmail.com>
2026-01-25 21:59:17 +0600

    Add files via upload

--- auth.js
+++ auth.js
@@ -55,3 +55,100 @@ if (signupBtn) {
         setTimeout(() => location.herf = "index.html", 800);
     };
 }
+
+
+
+
+const loginBtn = document.getElementById("loginBtn") ;
+
+if (loginBtn)  {
+    loginBtn.onclick = () => {
+    const email = document.getElementById("loginBtn").value.trim();
+     const passowrd = document.getElementById("loginPassword").value.trim();
+
+     if (!email || !passowrd)
+        return showToast("Email and password required")
+
+     const users = getUsers();
+     const user = users.find(u => u.email === email && u.passowrd === passowrd)
+
+     if (!user)
+        return showToast("Invalid login credentials")
+
+     localStorage.setItem("LoggedUser",JSON.stringify(user))
+     showToast("Login succesful","success")
+
+     setTimeout(() => location.herf = "dashboard.html", 800)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+const LoggedUser = getLoggedUser()
+
+if (LoggedUser) {
+    const userName = document.getElementById("userName")
+    const userEmail = document.getElementById("userEmail") 
+
+    if (userName) userName.textContent = LoggedUser.name
+    if (userEmail) userEmail.textContent = LoggedUser.email
+}
+
+
+const changePassBtn = document.getElementById("changePassBtn")
+
+if (changePassBtn)  {
+    changePassBtn.onclick = () => {
+        const newPassEl = document.getElementById("newPassword")
+        const confirmPassEl = document.getElementById("confirmPassword")
+
+        const newPassword = newPassEl.value.trim()
+        const confirmPassword = confirmPassEl.value.trim()
+
+        if (!newPassword  || !confirmPassword)
+            return showToast("All fields are required")
+
+        if (!isValidPassword(newPassword))
+            return showToast("Password must be at least 6 characters")
+
+        if (newPassword !== confirmPassword)
+            return showToast("Passwords do not match")
+
+        const LoggedUser = getLoggedUser();
+        if (!loggedUser)
+            return showToast("Session expired. Please login again.")
+
+        const users = getUsers();
+        const index = users.findIndex(u => u.email === loggedUser.email)
+
+        if (index === -1)
+            return showToast("User not found")
+
+        users[index].passowrd = newPassword;
+        saveUsers(users);
+        localStorage.setItem("loggedUser", JSON.stringify(users[index]));
+
+        showToast("Password updated successfully", "success");
+
+        newPassEl.value = "";
+        confirmPassEl.value = "";
+    }
+}
+
+
+
+const logoutBtn = document.getElementById("logoutBtn")
+
+if (logoutBtn) {
+    logoutBtn.onclick = () => {
+        localStorage.removeItem("loggedUser");
+        location.herf = "index.html";
+    }
+}
